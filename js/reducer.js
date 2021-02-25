b90aa2bf25298e2b5223e19a49131466ef9773bf
Thanapat <t.veerakajorn@gmail.com>
2021-02-25 14:23:45 -0500

    commit tree 56bfc8743c915912bc16c5896f64fc946553aac5

--- js/reducer.js
+++ js/reducer.js
@@ -1,13 +1,21 @@
+let state = {count: 0};
+
 function changeState(state, action){
-  switch (action.type) {
-    case 'INCREASE_COUNT':
-      return {count: state.count + 1}
-    default:
-      return state;
+    switch (action.type) {
+      case 'INCREASE_COUNT':
+        return {count: state.count + 1}
+      default:
+        return state;
+    }
   }
-}
 
-let state = {count: 0}
-let action = {type: 'INCREASE_COUNT'}
-
-changeState(state, action)
+  function render(){
+    document.body.textContent = state.count
+  }
+  
+  function dispatch(action){
+    state = changeState(state, action)
+    render()
+  }
+  
+render() 
